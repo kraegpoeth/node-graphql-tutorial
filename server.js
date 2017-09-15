66eaadf18cd65e759021447973199c6fd2a36ceb
Kristina Grönvall <kraegpoeth@gmail.com>
2017-09-15 19:41:25 +0200

    Added function to do Dice Rolls

--- server.js
+++ server.js
@@ -3,27 +3,48 @@ var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
 
 var schema = buildSchema(`
+  type RandomDie {
+    numSides: Int!
+    rollOnce: Int!
+    roll(numRolls: Int!): [Int]
+  }
+
   type Query {
-    rollDice(numDice: Int!, numSides: Int): [Int]
+    getDie(numSides: Int): RandomDie
   }
 `);
 
-var root = {
-  rollDice: function ({numDice, numSides}) {
+
+class RandomDie {
+  constructor(numSides) {
+    this.numSides = numSides;
+  }
+
+  rollOnce() {
+    return 1 + Math.floor(Math.random() * this.numSides);
+  }
+
+  roll({numRolls}) {
     var output = [];
-    for(var i = 0; i < numDice; i++) {
-      output.push(1 + Math.floot(Math.random() * (numSides || 6)));
+    for (var i = 0; i < numRolls; i++) {
+      output.push(this.rollOnce());
     }
     return output;
   }
-};
+}
+
+var root = {
+  getDie: function ({numDice, numSides}) {
+    return new RandomDie(numSides || 6);
+  }
+}
 
 var app = express();
-app.use('/graphiql', graphqlHTTP({
+app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
   graphiql: true
 }));
 
 app.listen(4000)
-console.log('gql server live on port localhost:4000/graphiql');
+console.log('gql server live on port localhost:4000/graphql');
