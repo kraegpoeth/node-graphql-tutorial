819a2476e0492ca51d07e45caa3452331f01d5b7
Kristina Grönvall <kraegpoeth@gmail.com>
2017-09-18 11:10:39 +0200

    mutation to add or update a message

--- server.js
+++ server.js
@@ -3,40 +3,60 @@ var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
 
 var schema = buildSchema(`
-  type RandomDie {
-    numSides: Int!
-    rollOnce: Int!
-    roll(numRolls: Int!): [Int]
+  input MessageInput {
+    content: String
+    author: String
+  }
+
+  type Message {
+    id: ID!
+    content: String
+    author: String
   }
 
   type Query {
-    getDie(numSides: Int): RandomDie
+    getMessage(id: ID!): Message
+  }
+
+  type Mutation {
+    createMessage(input: MessageInput): Message
+    updateMessage(id: ID!, input: MessageInput): Message
   }
 `);
 
-
-class RandomDie {
-  constructor(numSides) {
-    this.numSides = numSides;
-  }
-
-  rollOnce() {
-    return 1 + Math.floor(Math.random() * this.numSides);
-  }
-
-  roll({numRolls}) {
-    var output = [];
-    for (var i = 0; i < numRolls; i++) {
-      output.push(this.rollOnce());
-    }
-    return output;
+class Message {
+  constructor(id, {content, author}) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
   }
 }
 
+
+var fakeDatabase = {};
+
+
 var root = {
-  getDie: function ({numDice, numSides}) {
-    return new RandomDie(numSides || 6);
-  }
+  getMessage: function ({id}) {
+    if (!fakeDatabase[id]){
+    throw new Error('no message exists with id ' + id);
+    }
+    return new Message(id, fakeDatabase[id]);
+  },
+  createMessage: function ({input}) {
+    var id = require('crypto').randomBytes(10).toString('hex');
+
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
+  updateMessage: function ({id, input}) {
+    if(!fakeDatabase[id]) {
+      throw new Error('no message exists with id ' + id);
+    }
+    // Will replace all old data, could do partial update instead...
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
 }
 
 var app = express();
